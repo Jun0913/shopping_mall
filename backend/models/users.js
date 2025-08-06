9edb10740eed7b98bedbc09910c2904a5eade85a
cj0201 <dlckdwls1232@gmail.com>
2025-08-06 17:31:35 +0900

    mysql db 테이블에서 id-> list, name-> id 로 변경

--- backend/models/users.js
+++ backend/models/users.js
@@ -3,15 +3,16 @@ const db = require('../config/db');
 
 const createUsersTable = `
   CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    list INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    name VARCHAR(100) NOT NULL,
+    id VARCHAR(100) NOT NULL,
     role ENUM('user', 'admin') DEFAULT 'user',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
   );
 `;
+
 
 async function initUsersTable() {
   try {
